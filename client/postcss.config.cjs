17828cee3fc21c471f24047b25536b83244835af
grigdon <gabrielrigdon@gmail.com>
2025-11-06 13:26:05 -0500

    updated node modules...working now

--- client/postcss.config.cjs
+++ client/postcss.config.cjs
@@ -1,10 +1,6 @@
-// client/postcss.config.cjs
-
 module.exports = {
   plugins: {
-    // Tailwind CSS automatically finds your tailwind.config.js file
-    'tailwindcss': {},
-    // Autoprefixer is essential for adding vendor prefixes
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
