b6c7a76c705002595b5f2e0e82ba0a88f6a6f6fb
grigdon <gabrielrigdon@gmail.com>
2025-11-06 13:11:15 -0500

    update

---
+++ client/postcss.config.cjs
@@ -0,0 +1,10 @@
+// client/postcss.config.cjs
+
+module.exports = {
+  plugins: {
+    // Tailwind CSS automatically finds your tailwind.config.js file
+    'tailwindcss': {},
+    // Autoprefixer is essential for adding vendor prefixes
+    'autoprefixer': {},
+  },
+};
